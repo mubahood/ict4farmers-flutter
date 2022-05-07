b2c4fe5b285519c3d8e3388c2d040dfcf43d0438
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-05-07 13:11:04 +0300

    romina

--- lib/utils/AppConfig.dart
+++ lib/utils/AppConfig.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static String BASE_URL = "https://farmers.ugnews24.info";
+  static String BASE_URL = "http://app2.unffeict4farmers.org";
   static const String HomePage = "HomePage";
   static const String MyProductsScreen = "MyProductsScreen";
   static const String AppName = "UNFFE ICT4Farmers";
